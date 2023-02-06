33a4ad979d9fcfbc14fe54f12edf4d5c34b49840
softmarshmallow <woojoo@softmarshmallow.com>
2023-02-06 15:20:09 +0900

    update webpack cfg

--- extension/webpack.config.js
+++ extension/webpack.config.js
@@ -1,31 +1,30 @@
-// @ts-nocheck
+//@ts-check
 
 "use strict";
 
 const path = require("path");
-const webpack = require("webpack");
-const Dotenv = require("dotenv-webpack");
+const dotenv = require("dotenv-webpack");
 
-/**@type {import('webpack').Configuration}*/
-const config = {
-  target: "node", // vscode extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
+//@ts-check
+/** @typedef {import('webpack').Configuration} WebpackConfig **/
+
+/** @type WebpackConfig */
+const extensionConfig = {
+  target: "node", // VS Code extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
   mode: "none", // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
 
   entry: "./src/extension.ts", // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
   output: {
     // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
-    path: path.resolve(__dirname, "..", "dist"),
+    path: path.resolve(__dirname, "dist"),
     filename: "extension.js",
     libraryTarget: "commonjs2",
   },
-  plugins: [
-    new Dotenv(),
-    // new webpack.EnvironmentPlugin(["NODE_ENV", "DEBUG", "CORS_GRIDA_API_KEY"]),
-  ],
-  devtool: "nosources-source-map",
+  plugins: [new dotenv()],
   externals: {
     vscode: "commonjs vscode", // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
     keytar: "commonjs keytar",
+    // modules added here also need to be added in the .vscodeignore file
   },
   resolve: {
     // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
@@ -44,5 +43,9 @@ const config = {
       },
     ],
   },
+  devtool: "nosources-source-map",
+  infrastructureLogging: {
+    level: "log", // enables logging required for problem matchers
+  },
 };
-module.exports = config;
+module.exports = [extensionConfig];
